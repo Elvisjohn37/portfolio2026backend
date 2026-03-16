751a0e17f785a4cb26a773f9a5af572c1ebfb2cb
elvis <elvisjohncayetano37@gmail.com>
2026-03-17 00:47:09 +0800

    Added root

--- src/controllers/about-controllers.js
+++ src/controllers/about-controllers.js
@@ -4,8 +4,7 @@ import About from "./../models/about.js"
 const getName = async (req, res) => {
 	const { id } = req.params
 	try {
-		const name = await About.findById(id).select("firstName position")
-		console.log("name: ", name)
+		const name = await About.findById(id).select("firstName position about1 province country")
 		if (_.isEmpty(name)) return res.status(404).json({ message: "No data available", data: {}, success: false })
 		return res.status(200).json({ message: "", data: name, success: true })
 	} catch (err) {
