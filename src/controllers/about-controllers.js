4906708f9cd2cb4f35070ac4224f204d6b30a803
elvis <elvisjohncayetano37@gmail.com>
2026-03-17 10:50:50 +0800

    Added more about route

--- src/controllers/about-controllers.js
+++ src/controllers/about-controllers.js
@@ -1,7 +1,7 @@
 import _ from "lodash"
 import About from "./../models/about.js"
 
-const getName = async (req, res) => {
+const getHomeData = async (req, res) => {
 	const { id } = req.params
 	try {
 		const name = await About.findById(id).select("firstName position about1 province country")
@@ -12,4 +12,17 @@ const getName = async (req, res) => {
 	}
 }
 
-export { getName }
+const getAboutData = async (req, res) => {
+	const { id } = req.params
+	try {
+		const name = await About.findById(id).select(
+			"firstName middleName lastName position about2 degree school schoolYear",
+		)
+		if (_.isEmpty(name)) return res.status(404).json({ message: "No data available", data: {}, success: false })
+		return res.status(200).json({ message: "", data: name, success: true })
+	} catch (err) {
+		return res.status(500).json({ message: "Server error", data: {}, success: false })
+	}
+}
+
+export { getHomeData, getAboutData }
--- src/routes/about-routes.js
+++ src/routes/about-routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { getName } from "../controllers/about-controllers.js"
+import { getHomeData, getAboutData } from "../controllers/about-controllers.js"
 
 const router = Router()
 
-router.get("/:id", getName)
+router.get("/:id", getHomeData)
+
+router.get("/more-about/:id", getAboutData)
 
 router.get("/", (_req, res) => res.status(200).json({ message: "OKAY" }))
 
